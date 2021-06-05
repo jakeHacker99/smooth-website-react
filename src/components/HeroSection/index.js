95751b9a751e9976c2909cc3284f622b597aeed5
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-06-05 18:22:55 +0200

    impl HeroElements & ButtonElements

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,24 +1,47 @@
-import React from "react";
-import { HeroBg, HeroContainer, VideoBg } from "./HeroElements";
+import React, { useState } from "react";
+import {
+  HeroBg,
+  HeroContainer,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from "./HeroElements";
+import { Button } from "../ButtonElement";
 import Video from "../../videos/video.mp4";
 
 const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+  const onHover = () => {
+    setHover(!hover);
+  };
+
   return (
     <HeroContainer>
       <HeroBg>
-        <VideoBg autoPlay loop muted src={Video} type="video/mp4">
-          <HeroH1> Virtual Banking Made Easy</HeroH1>
-          <HeroP>
-            Sign up for a new account today and receive $250 in credit towards
-            your next payment.s
-          </HeroP>
-          <HeroBtnWrapper>
-            <Button to="signup">
-              Get startted {hover ? <ArrowForward /> : <ArrowRight />}
-            </Button>
-          </HeroBtnWrapper>
-        </VideoBg>
+        <VideoBg autoPlay loop muted src={Video} type="video/mp4"></VideoBg>
       </HeroBg>
+      <HeroContent>
+        <HeroH1> Virtual Banking Made Easy</HeroH1>
+        <HeroP>
+          Sign up for a new account today and receive $250 in credit towards
+          your next payment.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to="signup"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            primary="true"
+            dark="true"
+          >
+            Get startted {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
     </HeroContainer>
   );
 };
